96ec2e03361684f26cd11e78f954e4ce52c1ff74
ksl1989 <40713728+ksl1989@users.noreply.github.com>
2020-05-03 02:45:18 +0800

    Added 民間全民電視（民視）Formosa Television channels

--- index.php
+++ index.php
@@ -35,6 +35,15 @@ $channels_nowtv = array(
     'nowtv_zhibotai' => '331',
     'nowtv_xinwentai' => '332'
 );
+$channels_ftv = array(
+    'minshi_hd' => 'FTV',
+    'minshi_diyitai' => 'FMTV',
+    'minshi_taiwantai' => 'FTTV',
+    'minshi_xinwentai' => 'FTVNews',
+    'minshi_yingjutai' => 'FTVDrama',
+    'minshi_zongyitai' => 'FTVVariety',
+    'minshi_lvyoutai' => 'FTVTravel'
+);
 
 
 $channel = $_GET['channel'];
@@ -55,8 +64,14 @@ if (array_key_exists($channel, $channels_tvi)) {
     die;
 } elseif (array_key_exists($channel, $channels_nowtv)) {
     $stream_info_json = file_get_contents_curl('https://d1jithvltpp1l1.cloudfront.net/getLiveURL?channelno=' . $channels_nowtv[$channel] . '&mode=prod&audioCode=&format=HLS');
-    $stream_info = json_decode($stream_info_json , true);
+    $stream_info = json_decode($stream_info_json, true);
     $m3u8 = $stream_info['asset']['hls']['adaptive']['0'];
+    header('Location: ' . $m3u8);
+    die;
+} elseif (array_key_exists($channel, $channels_ftv)) {
+    $stream_info = file_get_contents_curl('https://app.4gtv.tv/Data/GetChannelURL_Mozai.ashx?ChannelNamecallback=channelname&Type=LIVE&ChannelName=' . $channels_ftv[$channel]);
+    $stream_info_json = $stream_info = json_decode(substr($stream_info, 12, -1), true);
+    $m3u8 = $stream_info_json['VideoURL'];
     header('Location: ' . $m3u8);
     die;
 } else {
